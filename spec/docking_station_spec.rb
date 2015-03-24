7d923653f21ceed42a8748e218760130b99cd618
Clint <clintp1@gmail.com>
2015-03-24 08:13:51 +0000

    added unit tests for bike class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,4 +3,8 @@ describe DockingStation do
   it 'DockingStation should respond to #release_bike' do
     expect(subject).to respond_to :release_bike
   end
+  it 'releases bikes that are not broken' do
+    bike = subject.release_bike
+    expect(bike).not_to be_broken
+  end
 end
